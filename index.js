e28db91f85ed3bf4fe1915d18f79cef4eb7e8df0
archiebalais <archieabalais@gmail.com>
2021-02-02 15:30:32 -0800

    index: Add delete route to server and button to view

--- index.js
+++ index.js
@@ -1,66 +1,108 @@
-const express = require('express');
-const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const fs = require('fs');
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const fs = require('fs')
+const methodOverride = require('method-override');
+
 
 // MiddleWare
 // this will help us use our layout file
-app.use(expressLayouts);
+app.use(expressLayouts)
 app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
 // for views use .ejs files
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.send('hi there');
+app.get('/', (req, res)=> {
+    res.send('Hi there')
 })
 
-// index view
+// Index View
 // this url: localhost:8000/dinosaurs
-app.get('/dinosaurs', (req, res) => {
-    let dinos = fs.readFileSync('./dinosaurs.json');
-    // take our data and put it in a more readable format
-    dinos = JSON.parse(dinos);
-    // in our views folder render this page
-    res.render('dinosaurs/index', { 
-        dinos: dinos 
-    })
-})
-
-// NEW view
-// most specific to least specific url path
-app.get('/dinosaurs/new', (req, res) => {
-    res.render('dinosaurs/new')
-})
-
-
-// SHOW view
-app.get('/dinosaurs/:index', (req, res) => {
+app.get('/dinosaurs', (req, res)=> {
     let dinos = fs.readFileSync('./dinosaurs.json')
     // take our data and put it in a more readable format
     dinos = JSON.parse(dinos)
-    // get the dino thats asked for
+    console.log(req.query.nameFilter)
+    let nameToFilterBy = req.query.nameFilter
+    // array method filter
+    console.log(nameToFilterBy)
+    
+    // if there is no submit of the form
+    // this will be undefined, and we will returnb all dinos
+    if (nameToFilterBy) {
+        const newFilteredArray = dinos.filter((dinosaurObj) => {
+            if (dinosaurObj.name.toLowerCase() === nameToFilterBy.toLowerCase()) {
+                return true
+            }
+        })
+        dinos = newFilteredArray
+    }
+    // console.log(dinos)
+    // in our views folder render this page
+    res.render('dinosaurs/index', {dinos: dinos} )
+})
+
+// NEW View
+// Most specific to least specific url path
+app.get('/dinosaurs/new', (req, res)=> {
+    res.render('dinosaurs/new')
+  })
+
+// SHOW View
+app.get('/dinosaurs/:index', (req, res)=> {
+    let dinos = fs.readFileSync('./dinosaurs.json')
+    // take our data and put it in a more readable format
+    dinos = JSON.parse(dinos)
+    // get the dino that's asked for
     // req.params.index
     const dino = dinos[req.params.index]
     res.render('dinosaurs/show', { dino })
 })
 
 
-// POST route, doesn't have a view 
 
-app.get('/dinosaurs', (req, res) => {
-    res.render('/dinosaurs/new')
-})
+// POST route, doesn't have a view
+app.post('/dinosaurs', (req, res)=> {
+    let dinos = fs.readFileSync('./dinosaurs.json')
+    // take our data and put it in a more readable format
+    dinos = JSON.parse(dinos)
+    // construct a new dino with our req.body values
+    const newDino = {
+        name: req.body.name,
+        type: req.body.type
+    }
+    // updates dinos with new dino
+    dinos.push(newDino)
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos))
 
-app.post('/dinosaurs', (req, res) => {
+    let lastIndex  = dinos.length -1
+    // get a request to /dinosaurs
+    res.redirect(`/dinosaurs/${lastIndex}`)
     // this is coming from our form submit
     // we are going to look at the req.body
-    console.log(req.body)
+    // console.log(req.body)
+})
+
+app.delete('/dinosaurs/:idx', (req, res) => {
+    const dinosaurs = fs.readFileSync('./dinosaurs.json');
+    const dinosaursArray = JSON.parse(dinosaurs);
+
+    //intermediate variable 
+    let idx = Number(req.params.idx) // what is this datatype? comes in as a string, change to integer
+    // remove the dinosaur
+    dinosaursArray.splice(idx, 1);
+    // save the dinosaur array into the dinosaurs.json file
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinosaursArray));
+    // redirect back to /dinosaurs route
+    res.redirect('/dinosaurs');
+
 })
 
 
 const PORT = process.env.PORT || 8000
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
 })
